c254f4887961dd1fb51f17d0c74e324f3b560364
julianaduranl <123585846+julianaduranl@users.noreply.github.com>
2024-03-16 22:49:09 -0500

    Empleado

--- src/main/java/uniandes/edu/co/proyecto/repositorio/EmpleadoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/EmpleadoRepository.java
@@ -1,5 +1,4 @@
 package uniandes.edu.co.proyecto.repositorio;
-
 import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,31 +9,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import uniandes.edu.co.proyecto.modelo.Empleado;
 
+
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
-    
-    @Query(value = "SELECT empleados.id AS empleado_id, empleados.cargo, usuarios.nombre AS usuario_nombre " +
-                   "FROM empleados INNER JOIN usuarios ON empleados.id = usuarios.id", nativeQuery = true)
+    @Query(value = "SELECT empleados.*, usuarios.nombre FROM empleados INNER JOIN usuarios ON empleados.id = usuarios.id", nativeQuery = true)
     Collection<Empleado> darEmpleados();
 
-    @Query(value = "SELECT empleados.id AS empleado_id, empleados.cargo, usuarios.id AS usuario_id " +
-                   "FROM empleados INNER JOIN usuarios ON empleados.id = usuarios.id WHERE empleados.id = :idEmpleado", nativeQuery = true)
+    @Query(value = "SELECT empleados.*, usuarios.id FROM empleados INNER JOIN usuarios ON empleados.id = usuarios.id WHERE id = :id", nativeQuery = true)
     Empleado darEmpleado(@Param("idEmpleado") Integer idEmpleado);
 
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO empleados (idEmpleado, cargo, idOficinaFK) VALUES (idUsuarios.nextval, :cargo, :idOficinaFK);" +
-                   "INSERT INTO usuarios (idEmpleado, nombre) VALUES (idUsuarios.CURRVAL, :nombre)", nativeQuery = true)
-    void insertarEmpleado(@Param("cargo") String cargo, @Param("idOficinaFK") Integer idOficinaFK, @Param("nombre") String nombre);
+    @Query(value = "INSERT INTO empleados VALUES (idUsuarios.nextval, :cargo, :idOficina);" +
+                    "INSERT INTO usuarios VALUES ((SELECT idUsuarios.CURRVAL FROM DUAL), :nombre)", nativeQuery = true)
+    void insertarEmpleado(@Param("cargo") String cargo, @Param("idOficina") Integer idOficina);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE empleados SET cargo = :cargo, idOficinaFK = :idOficinaFK WHERE id = :id;" +
-                   "UPDATE usuarios SET nombre = :nombre WHERE idEmpleado = :idEmpleado", nativeQuery = true)
-    void actualizarEmpleado(@Param("cargo") String cargo, @Param("idOficinaFK") Integer idOficinaFK, @Param("nombre") String nombre, @Param("idEmpleado") Integer idEmpleado);
-
+    @Query(value = "UPDATE empleados SET cargo= : cargo, idOficina = :idOficina  WHERE idEmpleado = :idEmpleado;" +
+                    "UPDATE usuarios SET nombre = :nombre WHERE idEmpleado = :idEmpleado", nativeQuery = true)
+    void actualizarEmpleado(@Param("cargo") String cargo, @Param("idOficina") Integer idOficina);
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM empleados WHERE idEmpleado = :idEmpleado;" +
-                   "DELETE FROM usuarios WHERE idEmpleado = :idEmpleado", nativeQuery = true)
-    void eliminarEmpleado(@Param("id") Integer id);
+                    "DELETE FROM usuarios WHERE idEmpleado = :idEmpleado", nativeQuery = true)
+    void eliminarEmpleado(@Param("idEmpleado") Integer idEmpleado);
 }
